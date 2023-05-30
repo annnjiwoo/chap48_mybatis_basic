e92ca0c564d10b5ff2df86b0c75936bf9841086f
annnjiwoo <annnjiwoo@gmail.com>
2023-05-30 17:39:54 +0900

    first commit;

---
+++ src/main/java/com/javalab/mybatis/domain/Employees.java
@@ -0,0 +1,29 @@
+package com.javalab.mybatis.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employees {
+	private Integer employeeId;
+	private String lastName;
+	private String firstName;
+	private String email;
+	private String phoneNumber;
+	private String hireDate;
+	private String jobId;
+	private Integer salary;
+	private Integer commissionPct;
+	private Integer managerId;
+	private Integer departmentId;
+
+	
+
+}
